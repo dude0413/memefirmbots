67c3887cf645ce0d5e1ab6d8beaf19598f203c0c
Mitchell Durbin <dude0413@gmail.com>
2019-02-04 20:17:56 -0500

    Update RMOeconfirm.py

--- RMOeconfirm.py
+++ RMOeconfirm.py
@@ -31,20 +31,21 @@ now = datetime.datetime.now()
 log_file_name = str(now.strftime('%m.%d') + '..' + str(random.randint(1,100)) + '.txt')
 log_file = open(log_file_name, 'w')
 invested_file_name = 'investedin.txt'
-invested_file = open(invested_file_name, 'w')
+invested_file = open(invested_file_name, 'w+')
 # Add the post_ids to the file #
 def append_to_invested_file(post_id):
     with open(invested_file_name, 'a') as I:
         I.write(post_id + '\n')
 # Log function #
-def log(input, bot_used):
-    message = str(bot_used) + ': ' + str(input) + '\n' + str(now.strftime('%m-%d %H:%M:%S'))
+def log(input, bot_used=executive_bot):
+    message = str(bot_used.user.me()) + ': ' + str(input) + '\n' + str(now.strftime('%m-%d %H:%M:%S'))
     print(message)
     with open(log_file_name, 'a') as l:
         l.write(message + '\n\n')
 
 # Finds the balance, invests into the post, and uses the log function to document #
 def invest(post_id, bot_name):
+    investment_boolean = True
     # Configure each bot #
     # Get balance #
     inbox_comment_reply_list = []
@@ -56,6 +57,7 @@ def invest(post_id, bot_name):
 
     balance = int(message_breakdown[-2].replace("**", "").replace(",", ""))
     # Invest and upvote post#
+    log(str(bot_name.user.me()) + ' has found a post and has upvoted it')
     investment_submission = bot_name.submission(post_id)
     investment_submission.upvote()
     for comment in investment_submission.comments:
@@ -64,62 +66,68 @@ def invest(post_id, bot_name):
             try:
                 comment.reply('!invest ' + str(amount))
                 append_to_invested_file(post_id)
+                log(str(bot_name.user.me()) + ' has invested in ' + str(post_id))
             except praw.exceptions.APIException:
                 print('Got the rate limit, exiting now...')
                 exit()
 #    announcement_message = 'An algorithm-based firm supports this meme.'
-    investment_submission.reply()
     log('Investing into ' + str(post_id), bot_name)
 
-
-
 while True:
-    # Tracking and Investing Mode - should take 1 hour #
-    for bot in bot_list:
-        for submission in subreddit.new(limit=15):
-            submission_id = submission.id
-            # Open the investments txt file and see if the submission id is in there, if not, continue
-            with open(invested_file_name) as r:
-                for i, l in enumerate(r):
-                    number_of_investments = i + 2
-                    str_number_of_investments = str(number_of_investments)
-                already_invested_investments = r.read().split('n')
-                already_invested_investments = list(filter(None, already_invested_investments))
-            if submission_id not in already_invested_investments:
-                submission_created_formatted = submission.created_utc
-                submission_time = datetime.datetime.utcfromtimestamp(submission_created_formatted)
-                age = (datetime.datetime.now() - submission_time)
-                upvotes_on_post = submission.ups
-                comments = submission.comments.list()
-                number_of_comments = len(comments)
-                submission_title = submission.title
+    investment_boolean = False
+    while investment_boolean == False:
+        log('Entering Searching Mode with my bots...')
+        # Tracking and Investing Mode - should take 1 hour #
+        for bot in bot_list:
+            log('Searching...', bot)
+            for submission in subreddit.new(limit=15):
+                submission_id = submission.id
+                # Open the investments txt file and see if the submission id is in there, if not, continue
+                with open(invested_file_name) as r:
+                    for i, l in enumerate(r):
+                        number_of_investments = i + 2
+                        str_number_of_investments = str(number_of_investments)
+                    already_invested_investments = r.read().split('n')
+                    already_invested_investments = list(filter(None, already_invested_investments))
+                if submission_id not in already_invested_investments:
+                    submission_created_formatted = submission.created_utc
+                    submission_time = datetime.datetime.utcfromtimestamp(submission_created_formatted)
+                    age = (datetime.datetime.now() - submission_time)
+                    upvotes_on_post = submission.ups
+                    comments = submission.comments.list()
+                    number_of_comments = len(comments)
+                    submission_title = submission.title
 
-                # If the post is less than 60 minutes old... #
-                # More than 25 minutes old #
-                time_boolean_twenty_five_old = age > datetime.timedelta(minutes=25)
-                # Less than 60 minutes old #
-                time_boolean_sixty = age < datetime.timedelta(minutes=60)
-                # Less than 10 minutes old #
-                time_boolean_ten = age < datetime.timedelta(minutes=10)
-                if time_boolean_twenty_five_old == False and upvotes_on_post > 35:
-                    invest(submission_id, bot)
-                elif time_boolean_sixty == True and time_boolean_twenty_five_old == True and 100 > upvotes_on_post > 35:
-                    invest(submission_id, bot)
-                elif time_boolean_sixty == True and 180 > upvotes_on_post > 70:
-                    invest(submission_id, bot)
-                elif time_boolean_ten == True and upvotes_on_post > 25:
-                    invest(submission_id, bot)
+                    # If the post is less than 60 minutes old... #
+                    # More than 25 minutes old #
+                    time_boolean_twenty_five_old = age > datetime.timedelta(minutes=25)
+                    # Less than 60 minutes old #
+                    time_boolean_sixty = age < datetime.timedelta(minutes=60)
+                    # Less than 10 minutes old #
+                    time_boolean_ten = age < datetime.timedelta(minutes=10)
+                    if time_boolean_twenty_five_old == False and upvotes_on_post > 35:
+                        invest(submission_id, bot)
+                    elif time_boolean_sixty == True and time_boolean_twenty_five_old == True and 100 > upvotes_on_post > 35:
+                        invest(submission_id, bot)
+                    elif time_boolean_sixty == True and 180 > upvotes_on_post > 70:
+                        invest(submission_id, bot)
+                    elif time_boolean_ten == True and upvotes_on_post > 25:
+                        invest(submission_id, bot)
+                    else:
+                        continue
                 else:
                     continue
-            else:
-                continue
+        log("My bots couldn't find anything... Sleeping for 10 minutes")
+        time.sleep(600)
+
             # Wait 4 hours and 1 minute to get the rewards for each of the bots #
+    log('Sleeping')
     time.sleep(14460)
     # Diagnostics mode #
+    # For every bot, copy and paste the final thing into the log file #
     for bot in bot_list:
         inbox_comment_reply_list = []
         for item in bot.inbox.comment_replies(limit=10):
             if item.author == "MemeInvestor_bot":
                 inbox_comment_reply_list.append(item.body)
         log(inbox_comment_reply_list[0], bot)
-
