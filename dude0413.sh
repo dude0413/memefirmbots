32bda24d19e5dcde998ac39f427fceda73259a44
Mitchell Durbin <dude0413@gmail.com>
2019-02-09 13:04:16 -0500

    Create dude0413.sh

---
+++ dude0413.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+python3 RMOfirmSOLO.py -b dude0413
+python3 RMOfirmSOLO.py -b dude0413
+python3 RMOfirmSOLO.py -b dude0413
+python3 RMOfirmSOLO.py -b dude0413
+python3 RMOfirmSOLO.py -b dude0413
+python3 RMOfirmSOLO.py -b dude0413
+python3 currentFirmStats.py
+python3 RMOfirmSOLO.py -b dude0413
+python3 RMOfirmSOLO.py -b dude0413
+python3 RMOfirmSOLO.py -b dude0413
+python3 RMOfirmSOLO.py -b dude0413
+python3 RMOfirmSOLO.py -b dude0413
+python3 RMOfirmSOLO.py -b dude0413
+python3 currentFirmStats.py
+python3 RMOfirmSOLO.py -b dude0413
+python3 RMOfirmSOLO.py -b dude0413
+python3 RMOfirmSOLO.py -b dude0413
+python3 RMOfirmSOLO.py -b dude0413
+python3 RMOfirmSOLO.py -b dude0413
+python3 RMOfirmSOLO.py -b dude0413
