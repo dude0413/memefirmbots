b131598b84caf6c061c2417535da14e5ec9a6042
Mitchell Durbin <dude0413@gmail.com>
2019-02-09 13:03:54 -0500

    Update RMOfirmSOLO.py

--- RMOfirmSOLO.py
+++ RMOfirmSOLO.py
@@ -1,15 +1,24 @@
 from package.config import *
 from package2.config import *
 from package3.config import *
-import time, random, datetime, os, argparse
-version = '0.9'
+from currentStats import merged_list, names_merged_list
+import time, random, os, datetime, argparse
+version = '1.0'
 
-bot = JDR_bot
+parser = argparse.ArgumentParser()
+parser.add_argument('-b', '--bot_used', help='you know what to do...')
+args = parser.parse_args()
 
-percentage_of_balance_invested = float(0.8)
-subreddit = bot.subreddit('MemeEconomy')
+for bot_name in names_merged_list:
+    if str(args.bot_used) == bot_name:
+        location = names_merged_list.index(bot_name)
+        bot = merged_list[location]
+    else:
+        continue
+percentage_of_balance_invested = float(0.9)
+
+subreddit = my_bot.subreddit('MemeEconomy')
 now = datetime.datetime.now()
-
 # Setting up Files #
 log_file_name = str(now.strftime('%m.%d') + '..' + str(random.randint(1,100)) + '.txt')
 log_file = open(log_file_name, 'w')
@@ -23,7 +32,7 @@ def append_to_invested_file(post_id):
     with open(invested_file_name, 'a') as I:
         I.write(post_id + '\n')
 # Log function #
-def log(input, bot_used=bot):
+def log(input, bot_used=my_bot):
     now = datetime.datetime.now()
     message = str(bot_used.user.me()) + ': ' + str(input) + '\n' + str(now.strftime('%m-%d %H:%M:%S'))
     print(message)
@@ -52,18 +61,24 @@ def invest(post_id, bot_name):
     for comment in investment_submission.comments:
         if comment.author == 'MemeInvestor_bot':
             amount = round(balance * percentage_of_balance_invested)
-            try:
-                comment.reply('!invest ' + str(amount))
-                append_to_invested_file(post_id)
-                log(str(bot_name.user.me()) + ' has invested in ' + str(post_id) + ' with ' + str(amount) + ' memecoins')
-            except praw.exceptions.APIException:
-                print('Got the rate limit, exiting now...')
-                exit()
+            now = datetime.datetime.now()
+            now_time = now.time()
+            if now_time >= datetime.time(22, 30) and now_time <= datetime.time(6, 00):
+                log('Investing with a lesser value because its nighttime', bot)
+                amount = round(balance * (percentage_of_balance_invested / 3))
+            else:
+                try:
+                    comment.reply('!invest ' + str(amount))
+                    append_to_invested_file(post_id)
+                    log(str(bot_name.user.me()) + ' has invested in ' + str(post_id) + ' with ' + str(amount) + ' memecoins')
+                except praw.exceptions.APIException:
+                    print('Got the rate limit, exiting now...')
+                    exit()
 #    announcement_message = 'An algorithm-based firm supports this meme.'
     log('Investing into ' + str(post_id), bot_name)
 
 while True:
-    log('Entering Searching Mode with my bots...')
+    log('Entering Searching Mode with me...')
     # Tracking and Investing Mode - should take 1 hour #
     investment_boolean = False
     while investment_boolean == False:
@@ -94,13 +109,13 @@ while True:
                 # Less than 10 minutes old #
                 time_boolean_ten = age < datetime.timedelta(minutes=10)
                 qualification_message = 'The post qualified because it was... '
-                if time_boolean_twenty_five_old == False and upvotes_on_post > 35:
+                if time_boolean_twenty_five_old == False and upvotes_on_post > 45:
                     invest(submission_id, bot)
                     log(qualification_message + 'Less than 25 minutes old and had more than 35 upvotes.')
                     investment_boolean = True
-                elif time_boolean_sixty == True and time_boolean_twenty_five_old == True and 100 > upvotes_on_post > 35:
+                elif time_boolean_sixty == True and time_boolean_twenty_five_old == True and 100 > upvotes_on_post > 55:
                     invest(submission_id, bot)
-                    log(qualification_message + 'Less than 60 minutes old and had between 100 and 35 upvotes.')
+                    log(qualification_message + 'Less than 60 minutes old and had between 100 and 55 upvotes.')
                     investment_boolean = True
                 elif time_boolean_ten == True and upvotes_on_post > 25:
                     invest(submission_id, bot)
@@ -110,13 +125,10 @@ while True:
                     continue
             else:
                 continue
-        log('Sleeping...', bot)
-        # Sleep for 8 minutes and 1 second, which should be enough time to
-        # cancel out the 'YOU'RE DOING TOO MUCH MESSAGE
-        time.sleep(481)
     # Wait 4 hours and 1 minute to get the rewards for each of the bots #
     log('Done with the investment cycle, going into diagnostics...')
-    time.sleep(14460)
+    time.sleep(14430)
+
     # Diagnostics mode #
     # For every bot, copy and paste the final thing into the log file #
     inbox_comment_reply_list = []
