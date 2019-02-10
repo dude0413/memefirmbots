6bc084e36a8af26014d7a4d36e53c16a7bb1db8d
Mitchell Durbin <dude0413@gmail.com>
2019-02-10 10:51:23 -0500

    Create memeslavebot5.sh

---
+++ memeslavebot5.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+python3 RMOfirmSOLO.py -b memeslavebot5
+python3 RMOfirmSOLO.py -b memeslavebot5
+python3 RMOfirmSOLO.py -b memeslavebot5
+python3 RMOfirmSOLO.py -b memeslavebot5
+python3 RMOfirmSOLO.py -b memeslavebot5
+python3 RMOfirmSOLO.py -b memeslavebot5
+python3 currentFirmStats.py
+python3 RMOfirmSOLO.py -b memeslavebot5
+python3 RMOfirmSOLO.py -b memeslavebot5
+python3 RMOfirmSOLO.py -b memeslavebot5
+python3 RMOfirmSOLO.py -b memeslavebot5
+python3 RMOfirmSOLO.py -b memeslavebot5
+python3 RMOfirmSOLO.py -b memeslavebot5
+python3 currentFirmStats.py
+python3 RMOfirmSOLO.py -b memeslavebot5
+python3 RMOfirmSOLO.py -b memeslavebot5
+python3 RMOfirmSOLO.py -b memeslavebot5
+python3 RMOfirmSOLO.py -b memeslavebot5
+python3 RMOfirmSOLO.py -b memeslavebot5
+python3 RMOfirmSOLO.py -b memeslavebot5
