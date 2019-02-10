92bc7d734b54f81dca603396fdbf3fe54409a4b5
Mitchell Durbin <dude0413@gmail.com>
2019-02-10 10:52:12 -0500

    Create skynetmemeeconbot.sh

---
+++ skynetmemeeconbot.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+python3 RMOfirmSOLO.py -b SkynetMemeEconBot
+python3 RMOfirmSOLO.py -b SkynetMemeEconBot
+python3 RMOfirmSOLO.py -b SkynetMemeEconBot
+python3 RMOfirmSOLO.py -b SkynetMemeEconBot
+python3 RMOfirmSOLO.py -b SkynetMemeEconBot
+python3 RMOfirmSOLO.py -b SkynetMemeEconBot
+python3 currentFirmStats.py
+python3 RMOfirmSOLO.py -b SkynetMemeEconBot
+python3 RMOfirmSOLO.py -b SkynetMemeEconBot
+python3 RMOfirmSOLO.py -b SkynetMemeEconBot
+python3 RMOfirmSOLO.py -b SkynetMemeEconBot
+python3 RMOfirmSOLO.py -b SkynetMemeEconBot
+python3 RMOfirmSOLO.py -b SkynetMemeEconBot
+python3 currentFirmStats.py
+python3 RMOfirmSOLO.py -b SkynetMemeEconBot
+python3 RMOfirmSOLO.py -b SkynetMemeEconBot
+python3 RMOfirmSOLO.py -b SkynetMemeEconBot
+python3 RMOfirmSOLO.py -b SkynetMemeEconBot
+python3 RMOfirmSOLO.py -b SkynetMemeEconBot
+python3 RMOfirmSOLO.py -b SkynetMemeEconBot
